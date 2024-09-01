ab830322e190bd3b143548f299536878d207b22f
OmarElati <5796@holbertonstudents.com>
2024-09-01 18:37:30 +0100

    Lets Begin

---
+++ holbegram/lib/screens/sigup_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Center(
+        child: Text('Signup Screen'),
+      ),
+    );
+  }
+}
---
+++ holbegram/lib/screens/upload_image_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UploadImageScreen extends StatelessWidget {
+  const UploadImageScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Upload Image'),
+      ),
+      body: Center(
+        child: Text('Upload Image Screen'),
+      ),
+    );
+  }
+}
